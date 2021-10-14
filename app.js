d06af5a71a3fa8a28cd33f976980ad4d31b81ff0
leilazarov <stoneiiilei@gmail.com>
2021-10-15 01:02:28 +1100

    router practice

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/random.text', (req, res) => {
+  res.send('random.text')
+})
+
+app.get('/ab?cd', (req, res) => {
+  res.send('ab?cd')
+})
+
+app.get('/ab+cd', function (req, res) {
+  res.send('ab+cd')
+})
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  const {userId, bookId} = req.params
+  console.log(userId)
+  console.log(bookId)
+  res.send(req.params)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
